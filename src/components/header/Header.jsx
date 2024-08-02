50f50c6f50ff28bd54aec4c74b898664c63b450a
OmarAhmed1417 <omarahmedramadan77@gmail.com>
2024-08-03 02:20:23 +0300

    Mobile Responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faTrash, faUser, faXmark } from '@fortawesome/free-solid-svg-icons'
 import './style/main.css';
 import { BrowserRouter as Router, Link,  } from 'react-router-dom';
 import { useGoogleLogin } from '@react-oauth/google';
@@ -8,6 +8,8 @@ import axios from 'axios';
 
 const Header = () => {
   const [userInfo, setUserInfo] = useState(null);
+  let [open,setopen]=useState(false)
+  let ref =useRef();
 
   const login = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
@@ -21,13 +23,28 @@ const Header = () => {
     onError: () => console.log('Login Failed'),
   });
 
+  let click=()=>{
+    if(open===true){
+      ref.current.style.transform = 'translateX(100%)';
+      ref.current.style.transition = 'transform 0.5s ease';
+      setopen(false);
+    }
+    else{
+      ref.current.style.transform = 'translateX(0%)';
+    ref.current.style.transition = 'transform 0.5s ease';
+      setopen(true);
+    }
+  }
     return ( 
         <>
 
     <header className=" bg-main_color p-4 ">
-        <div className="container mx-auto flex justify-between items-center ">
+        <div className="links container mx-auto flex justify-between items-center  ">
+          <div>
         <b className="text-2xl font-bold text-title">dangila</b>
-        <Router>
+          </div>
+          
+        <Router >
 
         <nav>
           <ul className="flex space-x-9">
@@ -56,6 +73,53 @@ const Header = () => {
         
         </div>
         </Router>
+
+      </div>
+
+
+
+      <div className="link2 container mx-auto  justify-between items-center  ">
+        
+          <div>
+        <b className="text-2xl font-bold text-title">dangila</b>
+          </div>
+          <div>
+          <FontAwesomeIcon icon={faBars} onClick={click} />
+          </div>
+      <div className="last"  ref={ref} onClick={click}>
+          <div className="exit text-left ">
+          <FontAwesomeIcon icon={faXmark} />
+        </div>
+
+        <Router >
+        <div className="icons  space-x-9">
+        
+       
+        <div className='flex flex-col text-center'>
+
+        <button onClick={() => login()}>       
+           <FontAwesomeIcon icon={faUser}  className='text-black_color pl-3'  />
+        </button>
+        <div>
+        {userInfo && <h5 className='text-black font-bold pl-2'>Hello {(userInfo.name)} </h5>}
+        </div>
+        </div>
+        
+        </div>
+        <nav>
+          <ul className=" space-x-9">
+            <li><Link  to="/" className="text-1xl font-semibold text-black link  p-1">Product</Link></li>
+            <li><Link  to="/" className="text-1xl font-semibold text-black link p-1">Story</Link></li>
+            <li><Link  to="/" className="text-1xl font-semibold text-black link p-1">Manufacturing</Link></li>
+            <li><Link  to="/" className="text-1xl font-semibold text-black link p-1">packaging</Link></li>
+            <li><Link  to="/" className="text-1xl font-semibold text-black link p-1">Team</Link></li>
+          </ul>
+        </nav>
+        
+        
+        </Router>
+        </div>
+
       </div>
     </header>
   
